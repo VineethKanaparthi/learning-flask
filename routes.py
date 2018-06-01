dba33c1f85de65c5efe75b4fa92108e8424dd40d
VineethKanaparthi <vineeth0025@gmail.com>
2018-06-01 14:51:04 +0530

    Added flask heroku

--- routes.py
+++ routes.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:kvrao1234@localhost:5432/learningflask'
 heroku = Heroku(app)
 with app.app_context():
+	db.app = app
     db.init_app(app)
 
 app.secret_key = "development-key"
